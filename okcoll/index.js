cd247b76f7d354930fb2f1b58de5d55c32942bd0
Tech 9-on <visheshmzn99@gmail.com>
2022-03-20 17:39:54 +0530

    some more changes

---
+++ okcoll/index.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+async function getGender(name){
+    let data = await axios.get(`https://api.genderize.io/?name=${name}`)
+
+    return  name
+}
+
+
+module.exports = getGender
